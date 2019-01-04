f2b3d47691d1238d2b172b143c097baabfb4426f
Lillian Ashmore <Lillian.Ashmore@bcm.edu>
2019-01-04 16:33:06 -0600

    added memory and memoryless permutation options for mle.getPermN and mle.getPermMovie.

--- R/mle.getPermMovie.r
+++ R/mle.getPermMovie.r
@@ -27,7 +27,7 @@
 #' thresholdDiff=0.01
 #' subset.nodes = names(G)[sample(1:length(G), 3)]
 #' mle.getPermMovie(subset.nodes, ig, output_filepath = getwd(), movie=TRUE)
-mle.getPermMovie = function(subset.nodes, ig, output_filepath, movie=TRUE, subset=FALSE) {
+mle.getPermMovie_memoryless = function(subset.nodes, ig, output_filepath, movie=TRUE, subset=FALSE) {
   if (subset) {
     # TODO :: Test on different sized graphs. Do we like this design decision to subset neighborhood to get better view of network walk????
     ig.subset = delete.vertices(ig, v=V(ig)$name[-which(V(ig)$name %in% names(unlist(neighborhood(ig, nodes=subset.nodes, order=1))))])
@@ -53,7 +53,7 @@ mle.getPermMovie = function(subset.nodes, ig, output_filepath, movie=TRUE, subse
     V(ig.subset)$label = rep("", length(igraphObjectG))
     current_node_set = NULL
     png(sprintf("%s/diffusionP1Movie%d.png", output_filepath, length(current_node_set)), 500, 500)
-    plot.igraph(ig.subset, layout=coords, vertex.color=V(ig.subset)$color,
+    plot.igraph(ig.subset, layout=coords, vertex.color=V(ig.subset)$color, edge.arrow.size=0.01,
                 vertex.label=V(ig.subset)$label, edge.width=20*abs(E(ig.subset)$weight),  mark.col="dark red",
                 mark.groups = current_node_set)
     title(sprintf("{%s}", paste(as.numeric(current_node_set %in% subset.nodes), collapse="")), cex.main=2)
@@ -110,7 +110,7 @@ mle.getPermMovie = function(subset.nodes, ig, output_filepath, movie=TRUE, subse
       if (movie==TRUE) {
         png(sprintf("%s/diffusionP1Movie%d_%d.png", output_filepath, n, length(current_node_set)), 500, 500)
         plot.igraph(ig.subset, layout=coords, vertex.color=V(ig.subset)$color,
-                    vertex.label=V(ig.subset)$label, edge.width=20*abs(E(ig.subset)$weight),
+                    vertex.label=V(ig.subset)$label, edge.width=20*abs(E(ig.subset)$weight), edge.arrow.size=0.01,
                     vertex.size=5+round(50*unlist(currentGraph), 0), mark.col="dark red",
                     mark.groups = current_node_set)
         title(sprintf("{%s}", paste(as.numeric(current_node_set %in% subset.nodes), collapse="")), cex.main=2)
@@ -127,4 +127,108 @@ mle.getPermMovie = function(subset.nodes, ig, output_filepath, movie=TRUE, subse
   return(permutationByStartNode)
 }
 
+
+mle.getPermMovie_memory = function(subset.nodes, ig, output_filepath, movie=TRUE, subset=FALSE) {
+  if (subset) {
+    # TODO :: Test on different sized graphs. Do we like this design decision to subset neighborhood to get better view of network walk????
+    ig.subset = delete.vertices(ig, v=V(ig)$name[-which(V(ig)$name %in% names(unlist(neighborhood(ig, nodes=subset.nodes, order=1))))])
+    V(ig.subset)$label.cex = 2
+    V(ig.subset)$label = rep("", length(V(ig.subset)$name))
+    tmp = get.adjacency(ig.subset, attr="weight")
+    tmp = abs(tmp)
+    igraphTestG = graph.adjacency(tmp, mode="undirected", weighted=TRUE)
+  } else {
+    ig.subset = ig
+  }
+  coords = layout.fruchterman.reingold(ig.subset)
+  igraphObjectG = vector(mode="list", length=length(V(ig.subset)$name))
+  names(igraphObjectG) = V(ig.subset)$name
+  igraphObjectG = lapply(igraphObjectG, function(i) i[[1]]=0)
+  degs = list(degree(ig.subset))
+  adjacency_matrix = list(as.matrix(get.adjacency(ig.subset, attr="weight")))
+
+  # Phase 1: Do adaptive permutations ahead of time for each possible startNode in subGraphS.
+  if (movie==TRUE) {
+    V(ig.subset)$color = rep("white", length(igraphObjectG))
+    V(ig.subset)$color[which(V(ig.subset)$name %in% subset.nodes)] = "green"
+    V(ig.subset)$label = rep("", length(igraphObjectG))
+    current_node_set = NULL
+    png(sprintf("%s/diffusionP1Movie%d.png", output_filepath, length(current_node_set)), 500, 500)
+    plot.igraph(ig.subset, layout=coords, vertex.color=V(ig.subset)$color, edge.arrow.size=0.01,
+                vertex.label=V(ig.subset)$label, edge.width=abs(E(ig.subset)$weight),  mark.col="dark red",
+                mark.groups = current_node_set)
+    title(sprintf("{%s}", paste(as.numeric(current_node_set %in% subset.nodes), collapse="")), cex.main=2)
+    legend("bottomleft", legend=c("Jackpot Nodes", "Drawn Nodes"), fill=c("green", "dark red"))
+    dev.off()
+  }
+  # Get all node names, so we know what all possible startNodes are.
+  permutationByStartNode = list()
+  bitStrings.pt = list()
+  for (n in 1:length(subset.nodes)) {
+    print(sprintf("Calculating permutation %d of %d...", n, length(subset.nodes)))
+    # Draw all nodes in graph
+    current_node_set = NULL
+    stopIterating=FALSE;
+    startNode = subset.nodes[n]
+    hits = startNode
+    currentGraph = igraphObjectG
+    while (stopIterating==FALSE) {
+      current_node_set = c(current_node_set, startNode)
+      sumHits = as.vector(matrix(0, nrow=length(V(ig.subset)$name), ncol=1))
+      names(sumHits) = names(igraphObjectG)
+      for (hit in 1:length(hits)) {
+        #For unseen nodes, clear probabilities and add probability (p0/#unseen nodes)
+        for (t in 1:length(currentGraph)) {
+          currentGraph[[t]] = 0; #set probabilities of all nodes to 0
+        }
+        #determine base p0 probability
+        baseP = p0/(length(currentGraph)-length(current_node_set));
+        for (t in 1:length(currentGraph)) {
+          if (!(names(currentGraph[t]) %in% current_node_set)) {
+            currentGraph[[t]] = baseP;  #set probabilities of unseen nodes to diffused p0 value, baseP
+          } else {
+            currentGraph[[t]] = 0;
+          }
+        }
+        p0_event = sum(unlist(currentGraph[!(names(currentGraph) %in% current_node_set)]))
+        currentGraph = graph.diffuseP1(p1, hits[hit], currentGraph, currentGraph[current_node_set], 1, verbose=FALSE)
+        p1_event = sum(unlist(currentGraph[!(names(currentGraph) %in% current_node_set)]))
+        if (abs(p1_event-1)>thresholdDiff) {
+          extra.prob.to.diffuse = 1-p1_event
+          currentGraph[names(current_node_set)] = 0
+          currentGraph[!(names(currentGraph) %in% names(current_node_set))] = unlist(currentGraph[!(names(currentGraph) %in% names(current_node_set))]) + extra.prob.to.diffuse/sum(!(names(currentGraph) %in% names(current_node_set)))
+        }
+        sumHits = sumHits + unlist(currentGraph)
+      }
+      sumHits = sumHits/length(hits)
+
+      #Set startNode to a node that is the max probability in the new currentGraph
+      maxProb = names(which.max(sumHits))
+
+      # Break ties: When there are ties, choose the first of the winners.
+      startNode = names(currentGraph[maxProb[1]])
+      if (all(subset.nodes %in% c(startNode,current_node_set))) {
+        current_node_set = c(current_node_set, startNode)
+        stopIterating = TRUE
+      }
+
+      if (movie==TRUE) {
+        png(sprintf("%s/diffusionP1Movie%d_%d.png", output_filepath, n, length(current_node_set)), 500, 500)
+        plot.igraph(ig.subset, layout=coords, vertex.color=V(ig.subset)$color,
+                    vertex.label=V(ig.subset)$label, edge.width=abs(E(ig.subset)$weight), edge.arrow.size=0.01,
+                    vertex.size=5+round(50*unlist(currentGraph), 0), mark.col="dark red",
+                    mark.groups = current_node_set)
+        title(sprintf("{%s}", paste(as.numeric(current_node_set %in% subset.nodes), collapse="")), cex.main=2)
+        legend("bottomleft", legend=c("Jackpot Nodes", "Drawn Nodes"), fill=c("green", "dark red"))
+        dev.off()
+      }
+    }
+
+    permutationByStartNode[[n]] = current_node_set
+    bitStrings.pt[[n]] = as.numeric(current_node_set %in% subset.nodes)
+  }
+  names(permutationByStartNode) = subset.nodes
+
+  return(permutationByStartNode)
+}
 
---
+++ R/mle.getPermN_memory.r
@@ -0,0 +1,82 @@
+#' Generate the "adaptive walk" node permutations, starting from a given perturbed variable
+#'
+#' This function calculates the node permutation starting from a given perturbed variable in a subset of variables in the background knowledge graph.
+#' @param n - The index (out of a vector of metabolite names) of the permutation you want to calculate.
+#' @keywords probability diffusion algorithm
+#' @keywords network walker algorithm
+#' @export mle.getPermN_memory
+#' @examples
+#' # Read in any network via its adjacency matrix
+#' tmp = matrix(1, nrow=100, ncol=100)
+#' for (i in 1:100) {
+#'   for (j in 1:100) {
+#'     tmp[i, j] = rnorm(1, mean=0, sd=1)
+#'   }
+#' }
+#' colnames(tmp) = sprintf("MolPheno%d", 1:100)
+#' ig = graph.adjacency(tmp, mode="undirected", weighted=TRUE, add.colnames="name")
+#' V(ig)$name = tolower(V(ig)$name)
+#' adjacency_matrix = list(as.matrix(get.adjacency(ig, attr="weight")))  # Must have this declared as a GLOBAL variable!!!!!
+#' # Set other tuning parameters
+#' p0=0.1  # 10% of probability distributed uniformly
+#' p1=0.9  # 90% of probability diffused based on edge weights in networks
+#' thresholdDiff=0.01
+#' G = vector(mode="list", length=length(V(ig)$name))
+#' names(G) = V(ig)$name
+#' # Get node permutations for graph
+#' perms = list()
+#' for (n in 1:length(G)) {
+#'   print(sprintf("Generating node permutation starting with node %s", names(G)[n]))
+#'   perms[[n]] = mle.getPermN_memory(n, G)
+#' }
+#' names(perms) = names(G)
+mle.getPermN_memory = function(n, G) {
+  all_nodes = names(G)
+  print(sprintf("Calculating permutation %d of %d.", n, length(all_nodes)))
+  current_node_set = NULL
+  stopIterating=FALSE
+  startNode = all_nodes[n]
+  hits = startNode
+  currentGraph = G
+  while (stopIterating==FALSE) {
+    current_node_set = c(current_node_set, startNode)
+    sumHits = as.vector(matrix(0, nrow=length(G), ncol=1))
+    names(sumHits) = names(G)
+    for (hit in 1:length(hits)) {
+      #For unseen nodes, clear probabilities and add probability (p0/#unseen nodes)
+      for (t in 1:length(currentGraph)) {
+        currentGraph[[t]] = 0; #set probabilities of all nodes to 0
+      }
+      #determine base p0 probability
+      baseP = p0/(length(currentGraph)-length(current_node_set));
+      for (t in 1:length(currentGraph)) {
+        if (!(names(currentGraph[t]) %in% current_node_set)) {
+          currentGraph[[t]] = baseP;  #set probabilities of unseen nodes to diffused p0 value, baseP
+        } else {
+          currentGraph[[t]] = 0;
+        }
+      }
+      p0_event = sum(unlist(currentGraph[!(names(currentGraph) %in% current_node_set)]))
+      currentGraph = graph.diffuseP1(p1, hits[hit], currentGraph, currentGraph[current_node_set], 1, verbose=FALSE)
+      p1_event = sum(unlist(currentGraph[!(names(currentGraph) %in% current_node_set)]))
+      if (abs(p1_event-1)>thresholdDiff) {
+        extra.prob.to.diffuse = 1-p1_event
+        currentGraph[names(current_node_set)] = 0
+        currentGraph[!(names(currentGraph) %in% names(current_node_set))] = unlist(currentGraph[!(names(currentGraph) %in% names(current_node_set))]) + extra.prob.to.diffuse/sum(!(names(currentGraph) %in% names(current_node_set)))
+      }
+      sumHits = sumHits + unlist(currentGraph)
+    }
+    sumHits = sumHits/length(hits)
+    #Set startNode to a node that is the max probability in the new currentGraph
+    maxProb = names(which.max(sumHits))
+    # Break ties: When there are ties, choose the first of the winners.
+    startNode = names(currentGraph[maxProb[1]])
+    if (length(c(startNode,current_node_set))>=(length(G))) {
+      current_node_set = c(current_node_set, startNode)
+      stopIterating = TRUE
+    }
+  }
+  return(current_node_set)
+}
+
+
