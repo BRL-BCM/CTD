2d1282b7c67d626ae1bf8c81d14d4a3597523a81
Lillian Ashmore <Lillian.Ashmore@bcm.edu>
2019-03-21 12:42:03 -0500

    Split up getPtBSbyK into memory vs. memoryless network walker.
    Split up getPtBSbyK into memory vs. memoryless network walker.

--- R/mle.getPtBSbyK_memoryless.r
+++ R/mle.getPtBSbyK_memoryless.r
@@ -1,11 +1,12 @@
 #' Generate patient-specific bitstrings from adaptive network walk.
 #'
-#' This function calculates the bitstrings (1 is a hit; 0 is a miss) associated with the adaptive network walk
-#' made by the diffusion algorithm trying to find the variables in the encoded subset, given the background knowledge graph.
+#' This function calculates the bitstrings (1 is a hit; 0 is a miss) associated with the memoryless network walker
+#' trying to find the variables in the encoded subset, given the background knowledge graph.
 #' @param S - A character vector of node names describing the node subset to be encoded.
 #' @param perms - The list of permutations calculated over all possible nodes, starting with each node in subset of interest.
+#' @param num.misses - The number of misses tolerated by the network walker before path truncation occurs.
 #' @return pt.byK - a list of bitstrings, with the names of the list elements the node names of the encoded nodes
-#' @export mle.getPtBSbyK
+#' @export mle.getPtBSbyK_memoryless
 #' @examples
 #' # Look at main_CTD.r script for full analysis script: https://github.com/BRL-BCM/CTD.
 #' # Get bitstrings associated with each patient's top kmx variable subsets
@@ -13,17 +14,34 @@
 #' ptBSbyK = list()
 #' for (pt in 1:ncol(data_mx)) {
 #'   S = data_mx[order(abs(data_mx[,pt]), decreasing=TRUE),pt][1:kmx]
-#'   ptBSbyK[[ptID]] = mle.getPtBSbyK(S, perms)
+#'   ptBSbyK[[ptID]] = mle.getPtBSbyK_memoryless(S, perms)
 #' }
-mle.getPtBSbyK = function(S, perms) {
+mle.getPtBSbyK_memoryless = function(S, perms, num.misses=NULL) {
+  if (is.null(num.misses)) {
+    num.misses = ceiling(log2(length(perms)))
+  }
   pt.byK = list()
   for (k in 1:length(S)) {
     sig.nodes = S[1:k]
     sig.nodes = sapply(sig.nodes, trimws)
     pt.bitString = list()
     for (p in 1:length(sig.nodes)) {
-      pt.bitString[[sig.nodes[p]]] = as.numeric(perms[[sig.nodes[p]]] %in% sig.nodes)
-      names(pt.bitString[[sig.nodes[p]]]) = perms[[sig.nodes[p]]]
+      miss = 0
+      for (ii in 1:length(perms[[sig.nodes[p]]])) {
+        ind_t = as.numeric(perms[[sig.nodes[p]]][ii] %in% sig.nodes)
+        if (ind_t==0) {
+          miss = miss + 1
+          if (miss >= num.misses) {
+            thresh = ii
+            break;
+          }
+        } else {
+          miss = 0
+        }
+        pt.bitString[[sig.nodes[p]]][ii] = ind_t
+      }
+      pt.bitString[[sig.nodes[p]]] = pt.bitString[[sig.nodes[p]]][1:thresh]
+      names(pt.bitString[[sig.nodes[p]]]) = perms[[sig.nodes[p]]][1:thresh]
       ind = which(pt.bitString[[sig.nodes[p]]] == 1)
       pt.bitString[[sig.nodes[p]]] = pt.bitString[[sig.nodes[p]]][1:ind[length(ind)]]
     }
@@ -40,5 +58,8 @@ mle.getPtBSbyK = function(S, perms) {
     }
     pt.byK[[k]] = pt.bitString[[which.min(bestInd)]]
   }
+  
   return(pt.byK)
 }
+
+
