61f83a718a8eebd94655d35a3851b8a7081296cf
Lillian Ashmore <Lillian.Ashmore@bcm.edu>
2019-04-06 17:27:48 -0500

    Corrected MSEA function to ORA
    Mislabeled ORA function as MSEA.

--- R/data.getORA_Metabolon.r
+++ R/data.getORA_Metabolon.r
@@ -7,11 +7,11 @@
 #' @param type - Either "p-value" or "z-score".
 #' @param gene.profile - Default set to NULL, meaning the default enrichment analysis only considers metabolites. However, if you have gene data, too, set
 #' this parameter to a character vector of the gene names with found variants in the patient's record. Gene IDs must be converted to Entrez Identifiers.
-#' @export data.getMSEA_Metabolon
+#' @export data.getORA_Metabolon
 #' @examples
 #'
 #' pathway.data = data.getMSEA_Metabolon(met.profile, threhold=3, "z-score", NULL)
-data.getMSEA_Metabolon = function(met.profile, threshold=3, type="zscore", gene.profile=NULL) {
+data.getORA_Metabolon = function(met.profile, threshold=3, type="zscore", gene.profile=NULL) {
   met.profile = met.profile[which(!(is.na(met.profile)))]
   if (type=="zscore") {
     perturbed.mets = met.profile[which(abs(met.profile) > threshold)]
@@ -19,7 +19,7 @@ data.getMSEA_Metabolon = function(met.profile, threshold=3, type="zscore", gene.
     perturbed.mets = met.profile[which(met.profile < 0.05)]
   }
   nms.perturbed.mets = unique(as.character(unlist(sapply(names(perturbed.mets), function(i) strsplit(i, split=";")))))
-  
+
   # The size of the population of total possible metabolites to draw from
   population = names(met.profile)
   paths.hsa = list.dirs(path="../extdata", full.names = FALSE)
@@ -28,7 +28,7 @@ data.getMSEA_Metabolon = function(met.profile, threshold=3, type="zscore", gene.
   pathway.data = data.frame(Pathway=character(), FDR=numeric(), Pvalue=numeric(), Hits=integer(), Size=integer(), stringsAsFactors = FALSE)
   for (pathway in 1:length(paths.hsa)) {
     load(sprintf("../extdata/RData/%s.RData", paths.hsa[pathway]))
-    
+
     pathway.compounds = V(ig)$label[which(V(ig)$shape=="circle")]
     pathCompIDs = unique(tolower(pathway.compounds[which(pathway.compounds %in% population)]))
     # q (sample successes), m (population successes), n (population failures), k (sample size)
@@ -48,10 +48,10 @@ data.getMSEA_Metabolon = function(met.profile, threshold=3, type="zscore", gene.
   pathway.data[,"FDR"] = p.adjust(pathway.data[,"Pvalue"], method="fdr")
   # Sort by FDR, then Pvalue
   pathway.data = pathway.data[order(pathway.data[,"FDR"], pathway.data[,"Pvalue"]),]
-  
+
   # Then, only return rows that have Pvalue < 0.25
   pathway.data = pathway.data[which(pathway.data[,"Pvalue"]<0.25),]
-  
+
   return(pathway.data)
 }
 
