51b3d94606082de818802434cda9b90e10357b07
Lillian Ashmore <Lillian.Ashmore@bcm.edu>
2019-04-27 14:02:57 -0500

    Improvements to documentation of datasets.
    Improvements to documentation of datasets.

---
+++ R/Wangler2017_EDTA.r
@@ -0,0 +1,25 @@
+#' A metabolomic map of Zellweger spectrum disorders reveals novel disease biomarkers.
+#'
+#' Global metabolic profiling obtained by untargeted mass spectrometry-based
+#' metabolomic platform for the detection of novel and known inborn errors of
+#' metabolism.  This untargeted approach collected z-score values for >650 unique
+#' compounds from human plasma.  Data set contains 19 individual plasma samples with
+#' confirmed biallelic pathogenic variants in the PEX1 gene. These samples revealed
+#' elevations in pipecolic acid and long-chain lysophosphatidylcholines, as well as
+#' an unanticipated reduction in multiple sphingomyelin species.
+#'
+#' @docType data
+#' @usage data(Wangler2017_EDTA)
+#'
+#'
+#' @keywords datasets
+#'
+#' @references M.F. Wangler, et al. Genetics in Medicine, 2018, 00
+#' (\href{https://www.ncbi.nlm.nih.gov/pubmed/29419819}{PubMed})
+#'
+#' @source \href{https://static-content.springer.com/esm/art:10.1038/gim.2017.262/MediaObjects/41436_2018_207_MOESM2_ESM.xlsx}{Dataset}
+#'
+#' @examples
+#' require(CTD)
+#' data(Wangler2017_EDTA)
+"Wangler2017_EDTA"
