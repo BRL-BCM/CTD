5993a5655ce442fb65d7b9df8a806b58bbb56224
lashmore <lillian.ashmore@bcm.edu>
2019-10-16 15:38:17 -0500

    System command to download Drive file.
    Based on system("wget --continue URL/filename.filetype") example.

--- vignette/r-env-setup-script.R
+++ vignette/r-env-setup-script.R
@@ -18,7 +18,7 @@ downloadGDriveFile <- function(id, out){
 
 #' R package cache (precompiled packages based on colab env)
 message("Download R package cache")
-downloadGDriveFile(out="CTD_0.0.0.9000.tar.gz", id="11FiOzQf7YFESIhYG4tFaL6odbnXDAHop")
+system("wget --continue https://drive.google.com/open?id=11FiOzQf7YFESIhYG4tFaL6odbnXDAHop")
 
 #' Unpack cache locally
 message("Unzipping R package cache")
