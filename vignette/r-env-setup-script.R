edc94b3f3c04ddcd08618add3a1ff3c9e466b0a9
lashmore <lillian.ashmore@bcm.edu>
2019-10-18 13:14:11 -0500

    Updates after meeting with ASHG workshop lead.
    Updates after meeting with ASHG workshop lead.

--- vignette/r-env-setup-script.R
+++ vignette/r-env-setup-script.R
@@ -6,12 +6,12 @@
 #'
 
 #' R package cache (precompiled packages based on colab env)
-message("Download R package cache")
-system("wget --continue https://drive.google.com/uc?export=download&id=11FiOzQf7YFESIhYG4tFaL6odbnXDAHop")
+message("Download R package caches")
+system("wget --continue https://drive.google.com/uc?export=download&id=1HjI0ee2ImcQvL9OiJyKIwkARqLTkDHe6")
 
 #' Unpack cache locally
 message("Unzipping R package cache")
-system("tar -xzf CTD_0.0.0.9000.tar.gz -C /", intern=TRUE)
+system("tar -xzf r_binaries.tar.gz -C /", intern=TRUE)
 
 #' Set correct library path
 .libPaths(Sys.getenv("R_LIBS_USER"))
