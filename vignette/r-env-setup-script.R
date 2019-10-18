01e3fdc0747312f5339dc5fd5ac030628f4624d6
lashmore <lillian.ashmore@bcm.edu>
2019-10-18 13:21:55 -0500

    Updates to downloading precompiled r_binaries
    Updates to downloading precompiled r_binaries

--- vignette/r-env-setup-script.R
+++ vignette/r-env-setup-script.R
@@ -5,9 +5,19 @@
 #' installs needed Ubuntu packages.
 #'
 
+downloadGDriveFile <- function(id, out){
+    system(paste0(
+        'wget --continue --load-cookies /tmp/cookies.txt ',
+            '"https://docs.google.com/uc?export=download&confirm=$(wget ',
+            '--quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate ',
+            "'https://docs.google.com/uc?export=download&id=", id, "' -O- | ",
+            "sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\\1\\n/p')&id=", id, '" ',
+            "-O ", out, " && rm -rf /tmp/cookies.txt"), intern=TRUE)
+}
+
 #' R package cache (precompiled packages based on colab env)
-message("Download R package caches")
-system("wget --continue https://drive.google.com/uc?export=download&id=1HjI0ee2ImcQvL9OiJyKIwkARqLTkDHe6")
+message("Download R package cache")
+downloadGDriveFile(out="r_binaries.tar.gz", id="1-VqZb_Cv63AH8ogWwhSp48DUowzv_GQf")
 
 #' Unpack cache locally
 message("Unzipping R package cache")
