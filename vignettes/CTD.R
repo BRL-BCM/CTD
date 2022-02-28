34b6a01908b421b3e5d28f5a64b94a56e13a93f3
Vladimir Kovacevic <upravljac@gmail.com>
2022-02-28 01:13:49 +0100

    Initial R script with command-line interface.

---
+++ vignettes/CTD.R
@@ -0,0 +1,157 @@
+#!/usr/bin/env Rscript --vanilla
+library(methods)
+library(argparser)
+require(huge)
+require(CTD)
+
+p <- arg_parser("Connect The Dots - Find most connected sub-graph from the set of graphs")
+# Add a positional argument
+p <- add_argument(p, "--dataset", help="Dataset file name")
+# Add a flag
+p <- add_argument(p, "--column_name", help="Name of the column containing concentrations", flag=TRUE)
+# Add another flag
+p <- add_argument(p, "--out", help="output file name", flag=TRUE)
+argv <- parse_args(p)
+
+print(argv$dataset)
+
+
+
+## I.II: Learn a graph from data.
+#Note all code chunks in sections I - IV may rely on lines in previous code
+#chunks, so do not empty your environment between code chunks.
+#```{r learn_graph}
+# Load the Miller2015_Heparin dataset
+data(Miller2015)  # TODO: Add support for user's input file instead od this
+# Only include metabolites that are present in >90% reference samples.
+fil.rate=as.numeric(Miller2015$`Times identifed in all 200 samples`[-1])/200
+names(fil.rate) = rownames(Miller2015)[-1]
+data_mx = Miller2015[,grep("IEM_", colnames(Miller2015))]
+data_mx = data_mx[which(fil.rate>0.90), ]
+dim(data_mx)
+# Remove any metabolites where any profile has a z-score > 1000.
+# These are likely imputed raw values that were not z-scored.
+rmMets = names(which(apply(data_mx, 1, function(i) any(i>20))))
+if (length(rmMets)>0) {
+  data_mx = data_mx[-which(rownames(data_mx) %in% rmMets),]
+}
+dim(data_mx)
+
+# Get data from all patients with Argininemia
+diags = Miller2015["diagnosis", grep("IEM", colnames(Miller2015))]
+arg_data = data_mx[,which(diags=="Argininemia")]
+# Add surrogate disease and surrogate reference profiles based on 1 standard
+# deviation around profiles from real patients to improve rank of matrix when
+# learning Gaussian Markov Random Field network on data. While surrogate
+# profiles are not required, they tend to learn less complex networks
+# (i.e., networks with less edges) and in faster time.
+ind = which(diags=="No biochemical genetic diagnosis")
+arg_data=data.surrogateProfiles(arg_data, 1, ref_data = data_mx[,ind])
+dim(arg_data)
+
+# Learn a Gaussian Markov Random Field model using the Graphical LASSO in
+# the R package "huge".
+# Select the regularization parameter based on the "STARS" stability
+# estimate.
+#This will take 30 seconds - 1 minute.
+arg = huge(t(arg_data), method="glasso")
+plot(arg)
+# This will take several minutes. For a faster option, you can use the
+# "ebic" criterion instead of "stars", but we recommend "stars".
+arg.select = huge.select(arg, criterion="stars")
+plot(arg.select)
+# This is the regularization parameter the STARS method selected.
+print(arg.select$opt.lambda)
+# This is the corresponding inverse of the covariance matrix that corresponds
+# to the selected regularization level.
+arg_icov = as.matrix(arg.select$opt.icov)
+# Remove all "self" edges, as we are not interested in self-relationships.
+diag(arg_icov) = 0
+rownames(arg_icov) = rownames(arg_data)
+colnames(arg_icov) = rownames(arg_data)
+# Convert adjacency matrices to an igraph object.
+ig_arg = graph.adjacency(arg_icov, mode="undirected", weighted=TRUE,
+                         add.colnames="name")
+print(ig_arg)
+
+
+
+
+# IV. The Encoding Process
+#We're going to go back to our data using the Arginase deficiency network
+#model, and the Miller et al (2015) dataset.
+## IV.0 Re-define the Arginase deficiency network model
+
+print(ig_arg)
+adj_mat = as.matrix(get.adjacency(ig_arg, attr="weight"))
+G=vector(mode="list", length=length(V(ig_arg)$name))
+G[1:length(G)] = 0
+names(G) = V(ig_arg)$name
+
+
+## IV.I Choose your node subset.
+
+# Maximum subset size to inspect
+kmx=15
+# Get our node subset associated with the $KMX highest perturbed (up or down)
+# in our first Arginase deficiency sample.
+S_arg = sort(abs(arg_data[,1]), decreasing=TRUE)[1:kmx]
+print(S_arg)
+
+
+## IV.II Get k node permutations.
+# Get the single-node encoding node ranks starting from each node in the subset
+# S_arg.
+ranks = list()
+for (n in 1:length(S_arg)) {
+    ind = which(names(G)==names(S_arg)[n])
+    # probability_difussion starting from node with index ind
+    ranks[[n]]=singleNode.getNodeRanksN(ind,G,p1=1.0,thresholdDiff=0.01,
+                                        adj_mat,S=names(S_arg),
+                                        num.misses=log2(length(G)),TRUE)
+}
+names(ranks) = names(S_arg)
+# Vector ranks contains encodings for each node in S_arg
+# TODO: Why ranks contain 87 rows instead of 15?!
+
+## IV.III Convert to bitstrings.
+
+# Get the bitstrings associated with the patient's perturbed metabolites in
+# "S_arg" based on the node ranks calculated in the previous step, "ranks".
+ptBSbyK = mle.getPtBSbyK(names(S_arg), ranks)
+
+
+## IV.IV Get encoding length of minimum length codeword.
+# arg_data is dataframe with diseases (and surrogates)
+# and z-values for each metabolite
+ind = which(colnames(arg_data) %in% names(diags))
+data_mx.pvals=apply(arg_data[,ind], c(1,2),
+                    function(i) 2*pnorm(abs(i), lower.tail=FALSE))
+ptID = "IEM_1006"
+res = mle.getEncodingLength(ptBSbyK, t(data_mx.pvals), ptID, G)
+# returns a subset of nosed that are highly connected
+ind.mx = which.max(res$d.score)
+res[ind.mx,]
+
+
+
+## IV.V Get p-value of variable length encoding vs. fixed length encoding.
+
+# You can interpret the probability assigned to this metabolite set by
+# comparing it to a null encoding algorithm, which uses fixed-length codes
+# for all metabolites in the set. The "d.score" is the difference in bitlength
+# between the null and alternative encoding models. Using the Algorithmic
+# Significance theorem, we can estimate the upper bounds on a p-value by
+# 2^-d.score.
+p_value_F = 2^-res[ind.mx,"d.score"]
+# All metabolites in S_arg
+names(S_arg)
+# Which metabolites were in the 8 metabolite subset of patient IEM_1006's
+# top 15 perturbed metabolites that had the above p-value?
+ptBSbyK[[ind.mx]] # all metabolites in the bitstring
+# just the F metabolites that are in S_arg that were were "found"
+F = names(which(ptBSbyK[[ind.mx]]==1))
+print(F)
+print(p_value_F)
+
+
