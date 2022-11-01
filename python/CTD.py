54a60ff19639cdacca786506acba493334c58482
NevenaaNikolic <nevenanikolic@Nevenas-MacBook-Pro.local>
2022-11-01 10:51:44 +0100

    update CTD.py

--- python/CTD.py
+++ python/CTD.py
@@ -8,6 +8,7 @@ from igraph import Graph
 from rpy2.robjects import r, pandas2ri, packages, globalenv, ListVector, StrVector
 from scipy.stats import norm
 import json
+# from tqdm import tqdm
 
 huge = packages.importr("huge")
 r.source("./CTD/R/data.surrogateProfiles.r")
@@ -23,15 +24,23 @@ r.source("./getNodeNames.R")
 pandas2ri.activate()
 
 p = argparse.ArgumentParser(description="Connect The Dots - Find the most connected sub-graph")
-p.add_argument("--experimental", help="Experimental dataset file name", default = '')  # data/example_argininemia/experimental.csv
-p.add_argument("--control", help="Control dataset file name", default = '')            # data/example_argininemia/control.csv
-p.add_argument("--adj_matrix", help="CSV with adjacency matrix", default = '')         # data/example_argininemia/adj.csv
-p.add_argument("--s_module", help="Comma-separated list or path to CSV of graph G nodes to consider when searching for the most connected sub-graph.")
-p.add_argument("--kmx", help="Number of highly perturbed nodes to consider. Ignored if S module is given.", default=15, type=int)
-p.add_argument("--present_in_perc_for_s", help="Percentage of patients having metabolite for selection of S module. Ignored if S module is given.", default=0.5, type=float)
+p.add_argument("--experimental", help="Experimental dataset file name",
+               default='')
+p.add_argument("--control", help="Control dataset file name", default='')  # data/example_argininemia/control.csv
+p.add_argument("--adj_matrix", help="CSV with adjacency matrix", default='')  # data/example_argininemia/adj.csv
+p.add_argument("--s_module",
+               help="Comma-separated list or path to CSV of graph G nodes to consider when searching for the most "
+                    "connected sub-graph.")
+p.add_argument("--kmx", help="Number of highly perturbed nodes to consider. Ignored if S module is given.", default=15,
+               type=int)
+p.add_argument("--present_in_perc_for_s",
+               help="Percentage of patients having metabolite for selection of S module. Ignored if S module is given.",
+               default=0.5, type=float)
 p.add_argument("--output_name", help="Name of the output JSON file.")
 p.add_argument("--out_graph_name", help="Name of the output graph adjecancy CSV file.")
-p.add_argument("--glasso_criterion", help="Graph-ical Lasso prediction of the graph criterion. 'stars' is default, 'ebic' is faster.", default='stars')
+p.add_argument("--glasso_criterion",
+               help="Graph-ical Lasso prediction of the graph criterion. 'stars' is default, 'ebic' is faster.",
+               default='stars')
 p.add_argument("-v", "--verbose", action="count", help="Enable verbose logging.")
 argv = p.parse_args()
 
@@ -45,9 +54,15 @@ if os.path.exists(argv.experimental):
     control_data = pd.read_csv(argv.control, index_col=0)
     target_patients = list(experimental_df.columns)
 
+    # TODO: Do we need surrogates?
+    # Add surrogate disease and surrogate reference profiles based on 1 standard
+    # deviation around profiles from real patients to improve rank of matrix when
+    # learning Gaussian Markov Random Field network on data. While surrogate
+    # profiles are not required, they tend to learn less complex networks
+    # (i.e., networks with less edges) and in faster time.
+
     # Prepare for calling R function
     data_surrogateProfiles = globalenv['data.surrogateProfiles']
-
     experimental_df = data_surrogateProfiles(data=experimental_df, std=1, ref_data=control_data)
 else:
     target_patients = np.nan
@@ -64,7 +79,8 @@ else:
     experimental_select = huge.huge_select(experimental, criterion=argv.glasso_criterion)
     adj_df = [values for (name, values) in experimental_select.items() if name == 'opt.icov'][0]
     np.fill_diagonal(adj_df, 0)
-    adj_df = pd.DataFrame(adj_df, index=experimental_df.index, columns=experimental_df.index)  # keep as DataFrame for now
+    adj_df = pd.DataFrame(adj_df, index=experimental_df.index,
+                          columns=experimental_df.index)  # keep as DataFrame for now
     if argv.out_graph_name:
         adj_df.to_csv(argv.out_graph_name, index=False)
 
@@ -92,7 +108,8 @@ if not argv.s_module:
     occurrences = Counter(S_set)
 
     # Keep in the S module the metabolites perturbed in at least 50% patients
-    S_perturbed_nodes = [node for node in occurrences if occurrences[node] >= len(target_patients) * argv.present_in_perc_for_s]
+    S_perturbed_nodes = [node for node in occurrences if
+                         occurrences[node] >= len(target_patients) * argv.present_in_perc_for_s]
 
 
 elif os.path.exists(argv.s_module):
@@ -118,8 +135,8 @@ ranks = {}
 # Walk through all the nodes in S module
 logging.debug('Get the single-node encoding node ranks starting from each node.')
 
-for node in S_perturbed_nodes:
-    ind = [i+1 for i in range(0, len(G_r.names)) if G_r.names[i] == node][0]
+for node in S_perturbed_nodes: # TODO: CONSIDER ADDING tqdm
+    ind = [i + 1 for i in range(0, len(G_r.names)) if G_r.names[i] == node][0]
     # Probability diffusion starting from node with index ind
     ranks[node] = singleNode_getNodeRanksN(
         n=ind,
@@ -152,7 +169,7 @@ if os.path.exists(argv.experimental):
     # specified z-score. sf generates normal distribution with mean=0, std=1,
     # which is exactly what z-scores are
 else:
-    data_mx_pvals = np.zeros((1,1))
+    data_mx_pvals = np.zeros((1, 1))
 
 # If we have here specific Patient ID the function will calculate
 # Fisher fishers.Info and varPvalue but we'll pass NULL for now
@@ -160,7 +177,6 @@ try:
     ptID = data_mx_pvals.columns[0]
 except AttributeError:
     ptID = r('NULL')
-
 
 # Prepare for calling R function
 mle_getEncodingLength = globalenv['mle.getEncodingLength']
@@ -172,7 +188,7 @@ highest_dscore_paths = res.iloc[ind_mx]
 # Locate encoding (F) with best d-score
 # Tiebreaker 1: If several results have the same d-score take one with longest BS
 ind_F = np.where(highest_dscore_paths['optimalBS'].str.len() ==
-                highest_dscore_paths['optimalBS'].str.len().max())
+                 highest_dscore_paths['optimalBS'].str.len().max())
 ind_F = highest_dscore_paths.iloc[ind_F]
 
 # Tiebreaker 2: Take the one with the largest subsetSize
@@ -186,7 +202,7 @@ F_info = res.loc[ind_F, :]
 # for all metabolites in the set. The "d.score" is the difference in bitlength
 # Significance theorem, we can estimate the upper bounds on a p-value by
 # 2^-d.score.
-p_value_F = 2.0**(-F_info.iloc[0]['d.score'])
+p_value_F = 2.0 ** (-F_info.iloc[0]['d.score'])
 
 # Prepare for calling R function
 get_node_names = globalenv['getNodeNames']
@@ -194,19 +210,17 @@ get_node_names = globalenv['getNodeNames']
 # All metabolites in S
 print(S_perturbed_nodes)
 # All metabolites in the bitstring
-print(get_node_names(ptBSbyK, int(ind_F)))
+print(get_node_names(ptBSbyK, int(ind_F))) # tmp workaround
 
 # Just the F metabolites that are in S_arg that were were "found"
 
 # F_arr = ptBSbyK.rx2(int(ind_F)) <- np.array of encodings
 # Fs = np.where(F_arr == 1) <- np.array of indices
-
-# WORKAROUND
-Fs = list(get_node_names(ptBSbyK, int(ind_F), found=True))
+Fs = list(get_node_names(ptBSbyK, int(ind_F), found=True)) # tmp workaround
 
 logging.debug('Set of highly-connected perturbed metabolites F = {} with p-value = {}'.format(Fs, p_value_F))
 
-kmcm_probability = 2.0**(-len(F_info.iloc[0]['optimalBS']))
+kmcm_probability = 2.0 ** (-len(F_info.iloc[0]['optimalBS']))
 optimal_bitstring = F_info.iloc[0]['optimalBS']
 
 out_dict = {
